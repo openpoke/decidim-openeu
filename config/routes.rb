a6489764d9d65ac9d68b9e3b8580af491c6b37df
David Igón <david@pokecode.net>
2025-07-09 15:29:55 +0200

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ require "sidekiq/cron/web"
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  mount Decidim::Core::Engine => "/"
+  namespace :admin do
+    resources :iframe, only: [:index]
+  end
+
   authenticate :user, ->(u) { u.admin? } do
     mount Sidekiq::Web => "/sidekiq"
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  namespace :admin do
-    resources :iframe, only: [:index]
-  end
+  mount Decidim::Core::Engine => "/"
 end
