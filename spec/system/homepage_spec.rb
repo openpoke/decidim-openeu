a586620cfd3d42178f7190cd8566c93b9fd7e516
David Igón <david@pokecode.net>
2025-07-15 10:01:04 +0200

    add spec for partner content block

--- spec/system/homepage_spec.rb
+++ spec/system/homepage_spec.rb
@@ -4,6 +4,9 @@ require "rails_helper"
 
 describe "Visit the home page", perform_enqueued: true do # rubocop:disable RSpec/DescribeClass
   let(:organization) { create(:organization) }
+  let!(:content_block) do
+    create(:content_block, organization:, scope_name: :homepage, manifest_name: :partner_logos)
+  end
 
   before do
     switch_to_host(organization.host)
@@ -12,5 +15,10 @@ describe "Visit the home page", perform_enqueued: true do # rubocop:disable RSpe
 
   it "renders the home page" do
     expect(page).to have_content("Home")
+  end
+
+  it "has the partners content block" do
+    expect(page).to have_content("Members")
+    expect(page).to have_content("Associated partners")
   end
 end
