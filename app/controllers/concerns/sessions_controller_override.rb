b05326b38c973945a6b39880e8e810a6d89c1278
elviabth <ejbenedith@gmail.com>
2025-09-29 15:15:16 +0200

    remove namespaces

--- app/controllers/concerns/sessions_controller_override.rb
+++ app/controllers/concerns/sessions_controller_override.rb
@@ -1,27 +1,23 @@
 # frozen_string_literal: true
 
-module Decidim
-  module Devise
-    class SessionsControllerOverride < ::Devise::SessionsController
-      def create
-        super do |user|
-          verify_users_after_sign_in(user)
-        end
-      end
-
-      private
-
-      def verify_users_after_sign_in(user)
-        record = CsvDatum.find_or_create_by(email: user.email, organization: user.organization)
-        return unless record
-
-        authorization = Decidim::Authorization.find_or_initialize_by(
-          user:,
-          name: "csv_census"
-        )
-
-        authorization.grant! unless authorization.granted?
-      end
+module SessionsControllerOverride
+  def create
+    super do |user|
+      verify_users_after_sign_in(user)
     end
+  end
+
+  private
+
+  def verify_users_after_sign_in(user)
+    record = Decidim::Verifications::CsvDatum.find_or_create_by(email: user.email, organization: user.organization)
+    return unless record
+
+    authorization = Decidim::Authorization.find_or_initialize_by(
+      user:,
+      name: "csv_census"
+    )
+
+    authorization.grant! unless authorization.granted?
   end
 end
