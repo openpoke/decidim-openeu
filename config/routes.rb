79478231f7b94cb068d6ffe0fdebbe23de4fa234
Ivan Vergés <ivan@pokecode.net>
2025-12-15 11:46:02 +0100

    remove overrides (#28)
    * remove overrides
    
    * update module
    
    * update module
    
    * update module
    
    * update module
    
    * queue adapter from module
    
    * update module
    
    * remove route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  namespace :admin do
-    resources :iframe, only: [:index]
-  end
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   mount Decidim::Core::Engine => "/"
 end
